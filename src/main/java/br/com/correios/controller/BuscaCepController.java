a3bdf71efaa29ffbe7e2bbccf55ea83e82c43136
Guilherme Rezende <guiga.rezende@hotmail.com>
2018-06-26 22:57:13 -0300

    Update BuscaCepController.java

--- src/main/java/br/com/correios/controller/BuscaCepController.java
+++ src/main/java/br/com/correios/controller/BuscaCepController.java
@@ -1,40 +1,3 @@
-/*package br.com.correios.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import br.com.correios.dto.BuscaCepDTO;
-import br.com.correios.repository.BuscaCepRepository;
-
-@Controller
-@RequestMapping("/buscaCep")
-public class BuscaCepController {
-	@Autowired
-	private BuscaCepRepository buscaCepRepository;
-
-	@GetMapping
-	public String buscaCepEndereco() {
-		return "BuscaCep";
-	}
-
-	@GetMapping(value="/buscaLocalidadeLogradouro")
-	public ModelAndView resultadoBuscaCep(@RequestParam("UF") String UF, @RequestParam("Localidade")String Localidade,@RequestParam("Logradouro") String Logradouro) {
-		
-		List<BuscaCepDTO> buscaCeps = buscaCepRepository.buscaPorUfLogradouroLocalidade(UF, Localidade, Logradouro);	
-		
-		ModelAndView mv = new ModelAndView("ResultadoBuscaCep");
-		mv.addObject("buscaCeps", buscaCeps);
-		return mv;
-	}
-
-} 
-*/
 package br.com.correios.controller;
 
 import java.util.List;
@@ -65,6 +28,28 @@ public class BuscaCepController {
 	@RequestParam ("Logradouro") String Logradouro,	RedirectAttributes attributes) {
 		List<BuscaCepDTO> buscaCeps = buscaCepRepository.buscaPorUfLogradouroLocalidade(UF,Localidade,Logradouro);
 		
+		
+		// PEDIDO PARA OBTENÇÃO DE CAMPOS USANDO O JAVA!
+		
+		/*if(UF.equals("")) {
+			ModelAndView mv = new ModelAndView("redirect:/buscaCep");
+			attributes.addFlashAttribute("mensagem1", " Selecione a UF");
+			return mv;
+		} else{
+			if(Localidade.equals("")){
+				ModelAndView mv = new ModelAndView("redirect:/buscaCep");
+				attributes.addFlashAttribute("mensagem2", "Informe o nome da cidade, município, distrito ou povoado !");
+				return mv;
+			}else {
+				if(Logradouro.equals("")) {
+					ModelAndView mv = new ModelAndView("redirect:/buscaCep");
+					attributes.addFlashAttribute("mensagem3", " Informe o logradouro !");
+					return mv;
+				}
+			}
+			
+		}
+		*/
 		
 		ModelAndView mv = new ModelAndView("ResultadoBuscaCep");
 		mv.addObject("buscaCeps", buscaCeps);
